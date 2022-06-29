4e96a423d2e7f34b1c5d0c2047efe06300e8dee8
AintInsane <93153427+AintInsane@users.noreply.github.com>
2022-06-29 18:13:47 +0300

    Delete _buildManifest.js

--- _buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["index-a811aacce13da638.js"],"/_error":["_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
