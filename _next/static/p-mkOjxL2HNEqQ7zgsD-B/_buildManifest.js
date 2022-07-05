bb87be7b4b4ac32f2b7a0646614678a767ce7be3
aintinsane <93153427+AintInsane@users.noreply.github.com>
2022-07-05 21:20:24 +0300

    Add files via upload
    21:20

---
+++ _next/static/p-mkOjxL2HNEqQ7zgsD-B/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0422f4eb4de12207.css","static/chunks/pages/index-1e83007cf82ba0a6.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
